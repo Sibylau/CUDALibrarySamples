7976cd49d12b44979610de325eb9f9501205f880
Sibylau <jl3952@cornell.edu>
2025-04-13 20:07:56 -0400

    build LtSgemm

---
+++ cuBLASLt/LtIgemmTensor/build/CMakeFiles/sample_cublasLt_LtIgemmTensor.dir/DependInfo.cmake
@@ -0,0 +1,20 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/work/shared/users/phd/jl3952/workspace/CUDALibrarySamples/cuBLASLt/LtIgemmTensor/sample_cublasLt_LtIgemmTensor.cu" "CMakeFiles/sample_cublasLt_LtIgemmTensor.dir/sample_cublasLt_LtIgemmTensor.cu.o" "gcc" "CMakeFiles/sample_cublasLt_LtIgemmTensor.dir/sample_cublasLt_LtIgemmTensor.cu.o.d"
+  "/work/shared/users/phd/jl3952/workspace/CUDALibrarySamples/cuBLASLt/LtIgemmTensor/main.cpp" "CMakeFiles/sample_cublasLt_LtIgemmTensor.dir/main.cpp.o" "gcc" "CMakeFiles/sample_cublasLt_LtIgemmTensor.dir/main.cpp.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cuBLASLt/LtIgemmTensor/build/CMakeFiles/sample_cublasLt_LtIgemmTensor.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/sample_cublasLt_LtIgemmTensor.dir/main.cpp.o"
+  "CMakeFiles/sample_cublasLt_LtIgemmTensor.dir/main.cpp.o.d"
+  "CMakeFiles/sample_cublasLt_LtIgemmTensor.dir/sample_cublasLt_LtIgemmTensor.cu.o"
+  "CMakeFiles/sample_cublasLt_LtIgemmTensor.dir/sample_cublasLt_LtIgemmTensor.cu.o.d"
+  "sample_cublasLt_LtIgemmTensor"
+  "sample_cublasLt_LtIgemmTensor.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CUDA CXX)
+  include(CMakeFiles/sample_cublasLt_LtIgemmTensor.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
